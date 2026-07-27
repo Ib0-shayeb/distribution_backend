51df583aba0889a9c1e0dd0517d06d7436383cf3
Ib0-shayeb <ibrahimowich1@gmail.com>
2026-07-27 13:30:26 +0200

    security: added basic http auth with permit all

--- src/main/java/com/example/distribution_backernd/configuration/ProjectConfig.java
+++ src/main/java/com/example/distribution_backernd/configuration/ProjectConfig.java
@@ -2,10 +2,12 @@ package com.example.distribution_backernd.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
@@ -24,5 +26,16 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(httpBasic -> {});
+
+        http.authorizeHttpRequests(auth ->
+                auth.anyRequest().permitAll()
+        );
+
+        return http.build();
     }
 }
