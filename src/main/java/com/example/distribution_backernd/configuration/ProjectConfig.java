f556ddb99210225a76fc2ddf5331bcee778b5d3a
Ib0-shayeb <ibrahimowich1@gmail.com>
2026-07-30 00:05:04 +0200

    security: switch to BCryptPasswordEndoder

--- src/main/java/com/example/distribution_backernd/configuration/ProjectConfig.java
+++ src/main/java/com/example/distribution_backernd/configuration/ProjectConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -43,7 +44,7 @@ public class ProjectConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
