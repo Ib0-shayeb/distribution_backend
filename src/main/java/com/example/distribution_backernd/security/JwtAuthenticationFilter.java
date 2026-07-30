fcac95fbc820b8a65001ca16e5fe39104e32bfef
Ib0-shayeb <ibrahimowich1@gmail.com>
2026-07-31 00:30:08 +0200

    security: JwtAuthenticationFilter: added fallback for EventSource authentication header in url parameter

--- src/main/java/com/example/distribution_backernd/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/distribution_backernd/security/JwtAuthenticationFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -29,16 +28,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             FilterChain filterChain
     ) throws ServletException, IOException {
 
-        final String authHeader = request.getHeader("Authorization");
-        final String jwt;
+        String jwt = null;
         final String username;
 
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+        // standard authorization header
+        final String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            jwt = authHeader.substring(7);
+        }
+        // Fallback: check for the token url parameter (needed for sse EventSource)
+        else {
+            String tokenParam = request.getParameter("token");
+            if (tokenParam != null && !tokenParam.trim().isEmpty()) {
+                jwt = tokenParam;
+            }
+        }
+
+        if (jwt == null) {
             filterChain.doFilter(request, response);
             return;
         }
-
-        jwt = authHeader.substring(7);
 
         try {
             username = jwtUtil.extractUsername(jwt);
