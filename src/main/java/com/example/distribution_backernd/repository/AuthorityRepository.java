1feb476d3ee3ca6ef6b47cbf5b52097443534254
Ib0-shayeb <ibrahimowich1@gmail.com>
2026-07-30 15:05:23 +0200

    added Authority repository

--- src/main/java/com/example/distribution_backernd/repository/AuthorityRepository.java
+++ src/main/java/com/example/distribution_backernd/repository/AuthorityRepository.java
@@ -1,4 +1,9 @@
 package com.example.distribution_backernd.repository;
 
-public interface AuthorityRepository {
+import com.example.distribution_backernd.model.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface AuthorityRepository extends JpaRepository<Authority, Integer> {
+    List<Authority> findByUsername(String username);
 }
