1130245866cc7234d96e2b0cac2ae605f6a614b7
Ib0-shayeb <ibrahimowich1@gmail.com>
2026-08-01 18:16:35 +0200

    serve manager.html with thymeleaf through a speing controller

---
+++ src/main/java/com/example/distribution_backernd/controller/ManagerController.java
@@ -0,0 +1,19 @@
+package com.example.distribution_backernd.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ManagerController {
+    @Value("${GOOGLE_MAPS_KEY}")
+    private String googleMapsKey;
+
+    @GetMapping("/admin")
+    public String getAdminDashboard(Model model) {
+        model.addAttribute("googleMapsKey", googleMapsKey);
+
+        return "manager";
+    }
+}
