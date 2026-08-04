2907bf46b8361b9333e70d5cfb48b003aac2d488
Ib0-shayeb <ibrahimowich1@gmail.com>
2026-08-04 20:56:04 +0200

    added new locationLog query for multiple users

--- src/main/java/com/example/distribution_backernd/repository/LocationLogRepository.java
+++ src/main/java/com/example/distribution_backernd/repository/LocationLogRepository.java
@@ -1,5 +1,6 @@
 package com.example.distribution_backernd.repository;
 
+import com.example.distribution_backernd.dto.FlatTripLogProjection;
 import com.example.distribution_backernd.model.LocationLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,15 +13,44 @@ import java.util.List;
 @Repository
 public interface LocationLogRepository extends JpaRepository<LocationLog, Integer> {
 
-    // Returns all location records for a user from start to end (DateTime)
-    @Query(value = "SELECT * FROM location_logs WHERE user_id = :userId AND recorded_at BETWEEN :start AND :end ORDER BY recorded_at ASC", nativeQuery = true)
+    @Query(value = """
+        SELECT l.* 
+        FROM location_logs l
+        JOIN trips t ON l.trip_id = t.id
+        WHERE t.user_id = :userId 
+          AND l.recorded_at BETWEEN :start AND :end 
+        ORDER BY l.trip_id ASC, l.recorded_at ASC
+        """, nativeQuery = true)
     List<LocationLog> findHistoryRange(
             @Param("userId") Integer userId,
             @Param("start") ZonedDateTime start,
             @Param("end") ZonedDateTime end
     );
 
-    // Pulls only user IDs active in the last 1 hour
-    @Query(value = "SELECT DISTINCT user_id FROM location_logs WHERE recorded_at >= NOW() - INTERVAL '1 hour' ORDER BY user_id ASC", nativeQuery = true)
+    @Query(value = """
+        SELECT DISTINCT t.user_id
+        FROM location_logs l
+        JOIN trips t ON l.trip_id = t.id
+        WHERE l.recorded_at >= NOW() - INTERVAL '1 hour'
+        ORDER BY t.user_id ASC
+        """, nativeQuery = true)
     List<Integer> findActiveUserIds();
+
+    @Query(value = """
+    SELECT 
+        t.user_id AS userId, 
+        l.trip_id AS tripId, 
+        l.latitude AS latitude, 
+        l.longitude AS longitude
+    FROM location_logs l
+    JOIN trips t ON l.trip_id = t.id
+    WHERE t.user_id IN (:userIds) 
+      AND l.recorded_at BETWEEN :start AND :end 
+    ORDER BY t.user_id ASC, l.trip_id ASC, l.recorded_at ASC
+    """, nativeQuery = true)
+    List<FlatTripLogProjection> findHistoryRangeForUsers(
+            @Param("userIds") List<Integer> userIds,
+            @Param("start") ZonedDateTime start,
+            @Param("end") ZonedDateTime end
+    );
 }
