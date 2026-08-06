355ad71aa25ff7e2f8723cda2496e00d2975ce4b
Ib0-shayeb <ibrahimowich1@gmail.com>
2026-08-06 16:43:18 +0200

    added new queries by fleetId

--- src/main/java/com/example/distribution_backernd/repository/UserRepository.java
+++ src/main/java/com/example/distribution_backernd/repository/UserRepository.java
@@ -3,9 +3,14 @@ package com.example.distribution_backernd.repository;
 import com.example.distribution_backernd.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
+
+    List<User> findByFleetId(Integer fleetId);
+    Optional<User> findByIdAndFleetId(Integer id, Integer fleetId);
 }
