d5c3fe72531d1a58af50e722a9e912f886d1d100
Ib0-shayeb <ibrahimowich1@gmail.com>
2026-08-10 20:21:33 +0200

    table name change

--- src/main/java/com/example/distribution_backernd/model/Checklist.java
+++ src/main/java/com/example/distribution_backernd/model/Checklist.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "checklist")
+@Table(name = "checklists")
 public class Checklist {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
