11339368d4aaec84f26871e3ed0082dd2a72f8dd
Ib0-shayeb <ibrahimowich1@gmail.com>
2026-09-10 12:07:09 +0200

    fix column name in findHistoryRangeForUsers query

--- src/main/java/com/example/distribution_backernd/repository/LocationLogRepository.java
+++ src/main/java/com/example/distribution_backernd/repository/LocationLogRepository.java
@@ -48,7 +48,7 @@ public interface LocationLogRepository extends JpaRepository<LocationLog, Intege
             l.trip_id AS tripId, 
             l.latitude AS latitude, 
             l.longitude AS longitude,
-            l.recordedAt AS recorderAt
+            l.recorded_at AS recorderAt
         FROM location_logs l
         JOIN trips t ON l.trip_id = t.id
         WHERE t.fleet_id = :fleetId 
